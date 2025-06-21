2dd72f6e0601571edf4d90cc0feaaa28a9e24e40
leonl <leonlof0@gmail.com>
2025-06-21 21:42:18 +0200

    Zwiebel passt

---
+++ src/main/java/kebab_simulator/model/meal/ingredients/IngredientOnion.java
@@ -0,0 +1,23 @@
+package kebab_simulator.model.meal.ingredients;
+
+
+import kebab_simulator.model.entity.impl.food.EntityOnion;
+import kebab_simulator.model.meal.IngredientModel;
+import kebab_simulator.model.meal.IngredientState;
+
+
+public class IngredientOnion extends IngredientModel {
+
+
+    private static IngredientOnion instance = new IngredientOnion();
+
+
+    public IngredientOnion() {
+        super(new IngredientState(false, 0, true, 5.0), EntityOnion.class);
+    }
+
+
+    public static IngredientOnion getInstance() {
+        return IngredientOnion.instance;
+    }
+}
